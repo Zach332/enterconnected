f39fcd18fd1f2363b636294fc1ecce74b90f9a79
Edward Noe <eln4jtt@virginia.edu>
2021-03-27 13:51:50 -0400

    Add Spring boot web app

---
+++ src/main/java/hoohacks2021/hoohacks2021/TestController.java
@@ -0,0 +1,13 @@
+package hoohacks2021.hoohacks2021;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    
+    @GetMapping("/api/test")
+    public String test() {
+        return "test";
+    }
+}
---
+++ src/test/java/hoohacks2021/hoohacks2021/Hoohacks2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package hoohacks2021.hoohacks2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hoohacks2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
