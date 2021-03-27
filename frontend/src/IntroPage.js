17e2fe9b69e14ce27db1e34630feaa190b199426
Jonathan Hail <absolutecos@gmail.com>
2021-03-27 18:44:16 -0400

    Changed the crossfade on intro to actually work

--- frontend/src/IntroPage.js
+++ frontend/src/IntroPage.js
@@ -1,26 +1,22 @@
-import Activity1 from "./images/activity1.jpeg"
+import "./IntroPage.css"
 export default function LoginLandingGithub() {
-    var background = Activity1
-
-    const handleInputChange = (event) => {
-        console.log(event.target.value)
-    }
-
     return (
-        <div style={{ 
-            backgroundImage: `linear-gradient(
-                rgba(0, 0, 0, 0.7), 
-                rgba(0, 0, 0, 0.7)
-                ),
-                url(${background})`,
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            width: '100vw',
-            height: '100vh',
-        }}>
-            <div className="d-flex flex-column align-items-center justify-content-center vh-100">
+        <div id="home" className="panel">
+            <div className="d-flex flex-column align-items-center justify-content-center vh-100" id="welcomeContent">
                 <h1 className="text-light">Welcome!</h1>
+            </div>
+            <div className="inner">
+                <div id="backgroundchange">
+                    <div className="backgroundimg" id="back1"/>
+                    <div className="backgroundimg" id="back2"/>
+                    <div className="backgroundimg" id="back3"/>
+                    <div className="backgroundimg" id="back4"/>
+                    <div className="backgroundimg" id="back5"/>
+                    <div className="backgroundimg" id="back6"/>
+                    <div className="backgroundimg" id="back7"/>
+                    <div className="backgroundimg" id="back8"/>
+                    <div className="backgroundimg" id="back9"/>
+                </div>
             </div>
         </div>
     );
