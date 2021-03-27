b9ddf2e6874ba800e221780d31ed9843b9161f20
Jonathan Hail <absolutecos@gmail.com>
2021-03-27 18:52:27 -0400

    Merged Login Page Changes

--- frontend/src/IntroPage.js
+++ frontend/src/IntroPage.js
@@ -1,8 +1,6 @@
 import "./IntroPage.css"
 import { toQuery } from "./Routing";
 export default function LoginLandingGithub() {
-    var background = Activity1
-
     const onClickGoogle = () => {
         window.location.href =
             "https://accounts.google.com/o/oauth2/v2/auth?" + googleParams;
@@ -20,20 +18,6 @@ export default function LoginLandingGithub() {
         <div id="home" className="panel">
             <div className="d-flex flex-column align-items-center justify-content-center vh-100" id="welcomeContent">
                 <h1 className="text-light">Welcome!</h1>
-
-
-            <div className="inner">
-                <div id="backgroundchange">
-                    <div className="backgroundimg" id="back1"/>
-                    <div className="backgroundimg" id="back2"/>
-                    <div className="backgroundimg" id="back3"/>
-                    <div className="backgroundimg" id="back4"/>
-                    <div className="backgroundimg" id="back5"/>
-                    <div className="backgroundimg" id="back6"/>
-                    <div className="backgroundimg" id="back7"/>
-                    <div className="backgroundimg" id="back8"/>
-                    <div className="backgroundimg" id="back9"/>
-                </div>
                 <button
                     type="btn btn-light"
                     onClick={onClickGoogle}
@@ -48,16 +32,27 @@ export default function LoginLandingGithub() {
                         fill="currentColor"
                         xmlns="http://www.w3.org/2000/svg"
                     >
-                        <path
-                            fillRule="evenodd"
-                            d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"
-                        ></path>
-                        <path
-                            fillRule="evenodd"
-                            d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
-                        ></path>
+                        <path fillRule="evenodd"
+                              d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
+                        <path fillRule="evenodd"
+                              d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
                     </svg>
                 </button>
+            </div>
+
+
+            <div className="inner">
+                <div id="backgroundchange">
+                    <div className="backgroundimg" id="back1"/>
+                    <div className="backgroundimg" id="back2"/>
+                    <div className="backgroundimg" id="back3"/>
+                    <div className="backgroundimg" id="back4"/>
+                    <div className="backgroundimg" id="back5"/>
+                    <div className="backgroundimg" id="back6"/>
+                    <div className="backgroundimg" id="back7"/>
+                    <div className="backgroundimg" id="back8"/>
+                    <div className="backgroundimg" id="back9"/>
+                </div>
             </div>
         </div>
     );
