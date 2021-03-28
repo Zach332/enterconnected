3231962c75dfbc82bda9793df49a02260e5f4f6d
Zachary <heidelzachary@gmail.com>
2021-03-28 10:20:23 -0400

    Add 'Scheduled' note for scheduled activities

--- frontend/src/ActivityAvailabilityCard.js
+++ frontend/src/ActivityAvailabilityCard.js
@@ -16,6 +16,11 @@ export default function ActivityAvailabilityCard( {activityAvailability} ) {
             <div style={{width: 250, height: 270}} className={"card bg-info" + (activityAvailability.happening && " border-success")}>
                 <div className="card-body">
                     <h5 className="card-title">{activityAvailability.activityName}</h5>
+                    {activityAvailability.happening && <div
+                                            className="badge rounded-pill btn-primary me-2"
+                                        >
+                                            Scheduled
+                                        </div>}
                     <p className="card-text">I'm available from {formatTime(activityAvailability.rangeStartTime).toString()} to {formatTime(activityAvailability.rangeEndTime).toString()}.</p>
                     <p className="card-text">This activity requires {activityAvailability.minimumNumberOfParticipants} people.</p>
                 </div>
