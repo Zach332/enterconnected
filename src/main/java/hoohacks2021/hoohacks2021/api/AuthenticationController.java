aefc4f2fc4a6aa66022e509293dddad87d7a0a32
Edward Noe <eln4jtt@virginia.edu>
2021-03-27 21:14:12 -0400

    Add cross origin

--- src/main/java/hoohacks2021/hoohacks2021/api/AuthenticationController.java
+++ src/main/java/hoohacks2021/hoohacks2021/api/AuthenticationController.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+@CrossOrigin
 @RestController
 public class AuthenticationController {
 
