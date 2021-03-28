6a4c7f79cc2baec2b60a9bc79f9c2408e6fa04b6
Edward Noe <eln4jtt@virginia.edu>
2021-03-27 20:24:54 -0400

    Add cross origin

--- src/main/java/hoohacks2021/hoohacks2021/api/TestController.java
+++ src/main/java/hoohacks2021/hoohacks2021/api/TestController.java
@@ -1,12 +1,14 @@
 package hoohacks2021.hoohacks2021.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import hoohacks2021.hoohacks2021.database.Database;
 import hoohacks2021.hoohacks2021.database.entity.User;
 
+@CrossOrigin
 @RestController
 public class TestController {
     
