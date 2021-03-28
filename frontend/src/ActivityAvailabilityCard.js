4e4770f2e2ea639664db46262dab9ab05bc30ad5
Zachary <heidelzachary@gmail.com>
2021-03-27 23:40:37 -0400

    Adjust card sizes

--- frontend/src/ActivityAvailabilityCard.js
+++ frontend/src/ActivityAvailabilityCard.js
@@ -3,9 +3,9 @@ export default function ActivityAvailabilityCard( {activityAvailability} ) {
     return (
         <div
             className="mx-auto list-group-item list-group-item-action m-1 rounded border bg-info"
-            style={{width: 300, height:450}}
+            style={{width: 280, height:290}}
         >
-            <div style={{width: 270, height: 430}} className="card list-group-item-action bg-info">
+            <div style={{width: 250, height: 270}} className="card list-group-item-action bg-info">
                 <div className="card-body">
                     <h5 className="card-title">{activityAvailability.activityName}</h5>
                     <p className="card-text">I'm available from {activityAvailability.rangeStartTime} to {activityAvailability.rangeEndTime}.</p>
