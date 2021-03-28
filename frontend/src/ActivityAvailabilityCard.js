43f4462a8f1dd146ba5a6d719b72b7064ce730bd
Zachary <heidelzachary@gmail.com>
2021-03-28 10:23:05 -0400

    Remove unneeded code

--- frontend/src/ActivityAvailabilityCard.js
+++ frontend/src/ActivityAvailabilityCard.js
@@ -3,11 +3,6 @@ import {formatTime} from "./formatTime";
 
 export default function ActivityAvailabilityCard( {activityAvailability} ) {
 
-    React.useEffect(() => {
-        console.log(formatTime(activityAvailability.rangeStartTime));
-    }, []);
-
-
     return (
         <div
             className={"mx-auto list-group-item m-1 rounded border bg-info" + (activityAvailability.happening && " border-success")}
@@ -17,10 +12,10 @@ export default function ActivityAvailabilityCard( {activityAvailability} ) {
                 <div className="card-body">
                     <h5 className="card-title">{activityAvailability.activityName}</h5>
                     {activityAvailability.happening && <div
-                                            className="badge rounded-pill btn-primary me-2"
-                                        >
-                                            Scheduled
-                                        </div>}
+                            className="badge rounded-pill btn-primary me-2"
+                        >
+                            Scheduled
+                        </div>}
                     <p className="card-text">I'm available from {formatTime(activityAvailability.rangeStartTime).toString()} to {formatTime(activityAvailability.rangeEndTime).toString()}.</p>
                     <p className="card-text">This activity requires {activityAvailability.minimumNumberOfParticipants} people.</p>
                 </div>
