7fb4598dec20cf81bf027665da8fcf3414083a93
Jonathan Hail <absolutecos@gmail.com>
2021-03-27 23:39:18 -0400

    add conversion from epoch to human readable time

--- frontend/src/ActivityAvailabilityCard.js
+++ frontend/src/ActivityAvailabilityCard.js
@@ -1,17 +1,25 @@
+import React from "react";
+import {formatTime} from "./formatTime";
+
 export default function ActivityAvailabilityCard( {activityAvailability} ) {
 
+    React.useEffect(() => {
+        console.log(formatTime(activityAvailability.rangeStartTime));
+    }, []);
+
+
     return (
-        <dir
+        <div
             className="mx-auto list-group-item list-group-item-action m-1 rounded border"
             style={{width: 300, height:450, backgroundColor:"#ECDDD9"}}
         >
             <div style={{width: 270, height: 430, backgroundColor:"#ECDDD9"}} className="card list-group-item-action">
                 <div className="card-body">
                     <h5 className="card-title">{activityAvailability.activityName}</h5>
-                    <p className="card-text">I'm available from {activityAvailability.rangeStartTime} to {activityAvailability.rangeEndTime}.</p>
+                    <p className="card-text">I'm available from {formatTime(activityAvailability.rangeStartTime).toString()} to {formatTime(activityAvailability.rangeEndTime).toString()}.</p>
                     <p className="card-text">This activity requires {activityAvailability.minimumNumberOfParticipants} people.</p>
                 </div>
             </div>
-        </dir>
+        </div>
     )
 }
---
+++ frontend/src/formatTime.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function formatTime( epoch ) {
+    return new Date(epoch);
+}
