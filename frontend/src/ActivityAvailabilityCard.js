c604877fc5ec139b73c8602aebc10a7d5e404eb5
Zachary <heidelzachary@gmail.com>
2021-03-27 23:04:34 -0400

    Adjust color of cards

--- frontend/src/ActivityAvailabilityCard.js
+++ frontend/src/ActivityAvailabilityCard.js
@@ -1,17 +1,17 @@
 export default function ActivityAvailabilityCard( {activityAvailability} ) {
 
     return (
-        <dir
-            className="mx-auto list-group-item list-group-item-action m-1 rounded border"
-            style={{width: 300, height:450, backgroundColor:"#ECDDD9"}}
+        <div
+            className="mx-auto list-group-item list-group-item-action m-1 rounded border bg-info"
+            style={{width: 300, height:450}}
         >
-            <div style={{width: 270, height: 430, backgroundColor:"#ECDDD9"}} className="card list-group-item-action">
+            <div style={{width: 270, height: 430}} className="card list-group-item-action bg-info">
                 <div className="card-body">
                     <h5 className="card-title">{activityAvailability.activityName}</h5>
                     <p className="card-text">I'm available from {activityAvailability.rangeStartTime} to {activityAvailability.rangeEndTime}.</p>
                     <p className="card-text">This activity requires {activityAvailability.minimumNumberOfParticipants} people.</p>
                 </div>
             </div>
-        </dir>
+        </div>
     )
 }
