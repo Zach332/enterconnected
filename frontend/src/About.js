d368e5c0b69b7417e0207fb3dcd0d6bc84934dd6
richardzhou571 <rmz4qx@virginia.edu>
2021-03-28 10:42:39 -0400

    Add headings to about page

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -5,7 +5,16 @@ export default function About() {
         <div>
             <Navbar />
             <div className="me-auto p-2 ms-5">
-                <h1>About</h1>
+                <h1 id="enterconnected">EnterConnected</h1>
+                <p>Looking for an easy way to organize activities with friends without having to worry about availability and interests? Looking to connect with friends, but unsure of the best way to reach out? EnterConnected is a free and easy way to create activities with your friends, eliminating the hassle of scheduling.</p>
+                <h2 id="inspiration">Inspiration</h2>
+                <h2 id="what-it-does">What it does</h2>
+                <h2 id="what-s-next-for-enterconnected">What's next for EnterConnected</h2>
+                <h2 id="challenges-we-ran-into">Challenges we ran into</h2>
+                <h2 id="accomplishments-we-re-proud-of">Accomplishments we're proud of</h2>
+                <h2 id="what-we-learned">What we learned</h2>
+                <h2 id="built-with">Built with</h2>
+                <h2 id="resources-used">Resources Used</h2>
             </div>
         </div>
     )
