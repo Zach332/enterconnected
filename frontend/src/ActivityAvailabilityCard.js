ea8272c4e1de76325c12ffa1a819deca32c8c913
Zachary <heidelzachary@gmail.com>
2021-03-27 23:58:00 -0400

    Put green border around scheduled activities

--- frontend/src/ActivityAvailabilityCard.js
+++ frontend/src/ActivityAvailabilityCard.js
@@ -10,10 +10,10 @@ export default function ActivityAvailabilityCard( {activityAvailability} ) {
 
     return (
         <div
-            className="mx-auto list-group-item list-group-item-action m-1 rounded border bg-info"
+            className={"mx-auto list-group-item list-group-item-action m-1 rounded border bg-info" + (activityAvailability.happening && " border-success")}
             style={{width: 280, height:290}}
         >
-            <div style={{width: 250, height: 270}} className="card list-group-item-action bg-info">
+            <div style={{width: 250, height: 270}} className={"card list-group-item-action bg-info" + (activityAvailability.happening && " border-success")}>
                 <div className="card-body">
                     <h5 className="card-title">{activityAvailability.activityName}</h5>
                     <p className="card-text">I'm available from {formatTime(activityAvailability.rangeStartTime).toString()} to {formatTime(activityAvailability.rangeEndTime).toString()}.</p>
