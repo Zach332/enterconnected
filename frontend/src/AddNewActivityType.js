2455ba699837a51f3331219f116f44fa92506b97
Jonathan Hail <absolutecos@gmail.com>
2021-03-27 20:28:50 -0400

    Added axios dependency and started new activity page

--- frontend/src/AddNewActivityType.js
+++ frontend/src/AddNewActivityType.js
@@ -1,11 +1,33 @@
 import Navbar from './Navbar';
+import React from "react";
+import axios from "axios";
 
 
 export default function AddNewActivityType() {
+    const [activities, setActivities] = React.useState([])
+
+    React.useEffect(() => {
+        axios
+            .get("https://hoohacks2021-308917.ue.r.appspot.com/api/activities")
+            .then((response) => {
+                setActivities(response.data)
+            });
+    }, []);
+
     return (
         <div>
             <Navbar />
-                New Activity Type page
+            <div style={{padding: "50px"}}>
+                <h2>Add New Activity</h2>
+
+                <input list="activeList" name="myBrowser" placeholder="Enter Name of Activity..." />
+                <datalist id="activeList">
+                    {activities.map((activity) => (
+                        <option key={activity} value={activity} />
+                    ))}
+                </datalist>
+
+            </div>
         </div>
     )
 }
