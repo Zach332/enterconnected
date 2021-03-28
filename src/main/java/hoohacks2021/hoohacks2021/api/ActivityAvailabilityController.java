67f6f0d52d1ab209f5a9fbac697b5e543b17cb03
Edward Noe <eln4jtt@virginia.edu>
2021-03-27 22:54:26 -0400

    Add CORS to Activity Availability

--- src/main/java/hoohacks2021/hoohacks2021/api/ActivityAvailabilityController.java
+++ src/main/java/hoohacks2021/hoohacks2021/api/ActivityAvailabilityController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import hoohacks2021.hoohacks2021.database.Database;
 import hoohacks2021.hoohacks2021.database.entity.ActivityAvailability;
 
+@CrossOrigin
 @RestController
 public class ActivityAvailabilityController {
     
