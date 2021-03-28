b2fdd585b1dee9c598660a629918e6cedcf6a4e7
Zachary <heidelzachary@gmail.com>
2021-03-27 21:59:16 -0400

    Added message regarding load times

--- frontend/src/Spinner.js
+++ frontend/src/Spinner.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 export default function Spinner() {
     return (
@@ -5,6 +6,8 @@ export default function Spinner() {
             <div className="spinner-border mt-5" role="status">
                 <span className="visually-hidden">Loading...</span>
             </div>
+            <div className="m-2"></div>
+            Please be patient. The server may be asleep; it can take ~30 seconds to wake up. After this period, feel free to <Link to="/">start over</Link>.
         </div>
     );
 }
